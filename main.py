2a5bad7102247878d261de8d36fe9c855e2c722b
Brandom <brandom.quispe2020@gmail.com>
2026-03-27 16:15:18 -0300

    feat: função cadastrar curso e add o aluno em cursos

--- main.py
+++ main.py
@@ -3,34 +3,123 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
 
-class Aluno(Base):
-    __tablename__ = "alunos"
-
-    id = Column(Integer,primary_key=True, autoincrement=True)
-    nome = Column(String(100), nullable=False)
-
-    def __repr__(self):
-        return f"ID: {self.id} - NOME: {self.nome}"
-    
-class Curso(Base):
-    __tablename__ = "cursos"
-
-    id = Column(Integer,primary_key=True, autoincrement=True)
-    nome = Column(String(100), nullable=False)
-
-    def __repr__(self):
-        return f"ID: {self.id} - CURSO: {self.nome}"
-    
-#Tabela Intermediaria
-incricoes = Table(
+#Tabela Intermediária
+inscricoes = Table(
     "inscricoes", #nome da tabela
     Base.metadata,
     Column("aluno_id", Integer, ForeignKey("alunos.id"), primary_key=True),
     Column("curso_id", Integer, ForeignKey("cursos.id"), primary_key=True),
-
 )
+
+# Tabelas curso e aluno
+class Aluno(Base):
+    __tablename__ = "alunos"
+
+    #Como cria uma coluna
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    #Relacionamento
+    cursos = relationship("Curso", secondary=inscricoes, back_populates="alunos")
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"ID: {self.id} - NOME: {self.nome}"
+
+class Curso(Base):
+    __tablename__ = "cursos"
+
+    #Como cria uma coluna
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    alunos = relationship("Aluno", secondary=inscricoes, back_populates="cursos")
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"ID: {self.id} - NOME: {self.nome}"
+   
+
+#Conexão com db
 engine = create_engine("sqlite:///gestao_escolar.db")
 
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bing=engine)
+Session = sessionmaker(bind=engine)
+
+
+#Criar
+def cadastrar_curso():
+    with Session() as session:
+        try:
+            #Criar o objeto curso
+            nome_curso = input("Digite o nome do curso: ").capitalize()
+            curso = Curso(nome=nome_curso)
+            #Adicionar no banco
+            session.add(curso)  
+            #Salvar
+            session.commit()        
+            print(f"Curso {nome_curso} cadastrado com sucesso!")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+
+# cadastrar_curso()
+
+def cadastrar_aluno():
+    with Session() as session:
+        try:
+            #Buscar o curso do aluno
+            nome_curso = input("Digite o nome do curso para cadastrar o aluno: ").capitalize()
+            curso = session.query(Curso).filter_by(nome= nome_curso).first()
+            if curso == None:
+                print(f"Nenhum curso encontrado com esse nome {nome_curso}")
+                return
+            else:
+                nome_aluno = input("Digite o nome do aluno para cadastrar: ").capitalize()
+                aluno = Aluno(nome=nome_aluno)
+                aluno.cursos.append(curso)
+
+                session.add(aluno)
+                session.commit()
+
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+#cadastrar_aluno()
+
+#Listar
+
+
+
+
+
+
+#Atualizar
+def adicionar_curso():
+    with Session() as session:
+        try:
+            #Buscar o curso do aluno
+            nome_curso = input("Digite o nome do curso para inserir o aluno: ").capitalize()
+            curso = session.query(Curso).filter_by(nome=nome_curso).first()
+            if curso == None:
+                print(f"Nenhum curso encontrado com esse nome {nome_curso}")
+                return
+            else:
+                nome_aluno = input("Digite o nome do aluno para cadastrar: ").capitalize()
+                aluno = session.query(Aluno).filter_by(nome=nome_aluno).first()
+                if aluno == None:
+                    print(f"Nenhum aluno cadastro com esse nome {nome_aluno}")
+                    return
+                else:
+                    aluno.cursos.append(curso)
+                    session.commit()
+                    print(f"Aluno registro com sucesso no curso {nome_curso}")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+adicionar_curso()
+
+#Deletar
+
+#funcao para buscar um aluno do mesmo jeito que eu busco o curso e depois adicionar o aluno e adicionar em outro curso 

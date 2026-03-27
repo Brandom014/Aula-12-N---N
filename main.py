919df6e74153a1c35375fc7b2598a53c86638c30
Brandom <brandom.quispe2020@gmail.com>
2026-03-27 13:51:45 -0300

    feat: add tabela aluno

---
+++ main.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+Base = declarative_base()
+
+class Aluno(Base):
+    __tablename__ = "alunos"
+
+    id = Column(Integer,primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    def __repr__(self):
+        return f"ID: {self.id} - NOME: {self.nome}"
